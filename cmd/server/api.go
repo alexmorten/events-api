f6b5e470e917f6f6b22b39c7c77e9ffce99a161f
alexmorten <alexandrius.martinus@gmail.com>
2019-04-01 17:38:23 +0200

    fix ElasticsearchAddress flag

--- cmd/server/api.go
+++ cmd/server/api.go
@@ -13,7 +13,7 @@ func main() {
 	config := api.ServerConfig{}
 
 	flag.StringVar(&config.Neo4jAddress, "neo4j_address", "bolt://0.0.0.0:7687", "address to neo4j")
-	flag.StringVar(&config.Neo4jAddress, "elastic_address", "http://0.0.0.0:9200", "address to elasticsearch")
+	flag.StringVar(&config.ElasticsearchAddress, "elastic_address", "http://0.0.0.0:9200", "address to elasticsearch")
 	flag.IntVar(&config.Port, "port", 3000, "port the server should listen on for http requests")
 	flag.BoolVar(&config.LazyInitializeElastic, "lazily_initialize_elastic", false, "if set to true, creating the connection to elastic_search will be defered until we make a call to it")
 	flag.Parse()
